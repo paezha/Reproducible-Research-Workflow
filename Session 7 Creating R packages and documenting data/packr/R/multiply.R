ac2881733ad1d50dea2b2968eeb8804439c18f99
Antonio Paez <paezha@gmail.com>
2019-10-25 20:26:52 -0400

    First complete draft of Notes for Session 7, including sample package packr

---
+++ Session 7 Creating R packages and documenting data/packr/R/multiply.R
@@ -0,0 +1,11 @@
+#' Multiply two numbers
+#'
+#' @param a A number
+#' @param b A number
+#' @return The product of \code{a} and \code{b}
+#' @examples
+#' multiply(2, 5)
+#' multiply(4, 8)
+multiply <- function(a, b){
+  a * b
+}
